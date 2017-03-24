8a47367e1200931dc9e12075d48e33924f42ec84
Axel <axelfuh@gmail.com>
2017-03-24 17:34:29 +0100

    Update js

--- main.js
+++ main.js
@@ -1,42 +1,52 @@
-var quoteMostrada,
-    autorMostrado,
-    quote = [],
-    autor = [];
-
-
-var request = new XMLHttpRequest(),
-    url = "https://raw.githubusercontent.com/afuh/Quote/master/quotes.json";
-
-request.open('GET', url);
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-  var data = this.response;
-  quote = data.quotes;
-  autor = data.autores;
-}
-
-function randomQ() {
-    var index = Math.floor(Math.random() * quote.length);
-    quoteMostrada = quote[index];
-    document.getElementById('quotes').innerHTML = quote[index];
-
-    index = Math.floor(Math.random() * autor.length);
-    autorMostrado = autor[index];
-    document.getElementById('autores').innerHTML = autor[index];
-}
-
-function shareTweet(){
-    window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent('"'+ quoteMostrada + '" ' + "– "+ autorMostrado +"."));
-}
-
 window.onload = function() {
-   document.getElementById("hit").addEventListener("click", randomQ, false);
-   document.getElementById("tweet").addEventListener("click", shareTweet, false);
+  app.init();
+};
 
-    function delay() {
-    setTimeout(randomQ, 200);
-  }
-  delay();
+var app = {
+  init: function(){
+      this.request();
+      this.buttons();
+
+      function delay() {
+          setTimeout(app.render, 300);
+        }
+      delay();
+  },
+  cache: function(q, a){
+      this.quote = q;
+      this.autor = a;
+      this.tweetQuote;
+      this.tweetAutor;
+  },
+  request: function() {
+      var request = new XMLHttpRequest(),
+          url = "https://raw.githubusercontent.com/afuh/Quote/master/quotes.json";
+      request.open('GET', url);
+      request.responseType = 'json';
+      request.send();
+      request.onload = function() {
+          app.cache(this.response.quotes, this.response.autores);
+    }
+  },
+  randomQuote: function() {
+      var i = Math.floor(Math.random() * app.quote.length)
+      app.cache.tweetQuote = app.quote[i];
+      return app.quote[i];
+  },
+  randomAutor: function() {
+      var i = Math.floor(Math.random() * app.autor.length);
+      app.cache.tweetAutor = app.autor[i];
+      return app.autor[i];
+  },
+  buttons: function() {
+      document.getElementById("getQuote").addEventListener("click", this.render);
+      document.getElementById("tweet").addEventListener("click", this.tweet);
+  },
+  render: function() {
+      document.getElementById('quotes').innerHTML = app.randomQuote();
+      document.getElementById('autores').innerHTML = app.randomAutor();
+  },
+  tweet: function(){
+      window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent('"'+ app.cache.tweetQuote + '" ' + "– "+   app.cache.tweetAutor +"."));
+  },
 };
